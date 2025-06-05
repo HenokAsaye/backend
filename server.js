b32e3184f455c75e710251e29a4172556b8e7879
Henok Asaye <henokasaye77@gmail.com>
2025-06-05 17:45:43 +0300

    modify the port number

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import connectToDb from './config/dbconfig.js';
+
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectToDb();
+    app.listen(PORT, () => {
+      console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
