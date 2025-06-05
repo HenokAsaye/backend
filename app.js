2d747720b51bb6b395c291298068f97f4f9f62fc
Henok Asaye <henokasaye77@gmail.com>
2025-06-05 18:03:47 +0300

    make it to serve static files

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 import express from "express";
 import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
 import taskRoutes from "./routes/taskRoute.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-app.use(cors()); 
-app.use(express.json()); 
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API routes
 app.use('/api/tasks', taskRoutes);
+
+// Root route to serve the index.html page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 export default app;
